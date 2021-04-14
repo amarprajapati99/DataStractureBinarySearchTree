655a84cfe3852fc483547c136478852437b1b70f
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-14 12:38:06 +0530

    To create the binary tree shown by the given figure

--- src/test/java/com/binarysearchtree/datastrature/BinarySearchTreeTest.java
+++ src/test/java/com/binarysearchtree/datastrature/BinarySearchTreeTest.java
@@ -2,7 +2,8 @@ package com.binarysearchtree.datastrature;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-/* @Description- To create a BST by adding 56 and then adding 30 and 70. */
+/* @Description- To create a BST by adding 56 and then adding 30 and 70.
+*  To create the binary tree shown by the given figure */
 public class BinarySearchTreeTest {
     @Test
     public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree() {
@@ -12,5 +13,25 @@ public class BinarySearchTreeTest {
         myBinaryTree.add(70);
         int size = myBinaryTree.getSize();
         Assertions.assertEquals(3, size);
+    }
+
+    @Test
+    public void givenNumbersWhenAddedToBinaryTreeShouldReturnSizeThirteen() {
+        BinarySearchTree<Integer> myBinaryTree = new BinarySearchTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(11);
+        myBinaryTree.add(65);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        int size = myBinaryTree.getSize();
+        Assertions.assertEquals(13, size);
     }
 }
